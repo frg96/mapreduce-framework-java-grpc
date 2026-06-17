1d37a6d2511b9f7fb7ff8d4d7297028f8791dec7
Franklin George <franklingeorge96@gmail.com>
2026-06-17 09:25:29 -0500

    Added WordCountTasks with implementation for word count Mapper and Reducer

---
+++ src/test/java/com/frg96/mapreduce/wordcount/WordCountTasks.java
@@ -0,0 +1,52 @@
+package com.frg96.mapreduce.wordcount;
+
+import com.frg96.mapreduce.api.*;
+
+import java.util.List;
+
+public class WordCountTasks {
+    private WordCountTasks() {}
+
+    public static void register(String appId) {
+        boolean registered = TaskRegistry.register(appId, new TaskFactory() {
+            @Override
+            public Mapper createMapper() {
+                return new WordCountMapper();
+            }
+
+            @Override
+            public Reducer createReducer() {
+                return new WordCountReducer();
+            }
+        });
+
+        if(!registered) {
+            throw new IllegalStateException("Failed to register app_id: " + appId);
+        }
+    }
+
+    private static final class WordCountMapper implements Mapper {
+        @Override
+        public void map(String inputLine, MapperContext context) {
+            String[] words = inputLine.split("[\\s,.\"']+");
+
+            for(String word: words) {
+                if(!word.isEmpty())
+                    context.emit(word, "1");
+            }
+        }
+    }
+
+    private static final class WordCountReducer implements Reducer {
+        @Override
+        public void reduce(String key, List<String> values, ReducerContext context) {
+            int sum = 0;
+
+            for(String value: values) {
+                sum += Integer.parseInt(value);
+            }
+
+            context.emit(key, Integer.toString(sum));
+        }
+    }
+}
